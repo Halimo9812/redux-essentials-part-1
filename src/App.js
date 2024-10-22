f18485a2670c0e47cf0fc1eb73d460fcda842e32
Halimo9812 <Halimoaali98@gmail.com>
2024-10-22 15:43:05 -0400

    part two

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import {
 import { Navbar } from './app/Navbar'
 import { PostsList } from './features/posts/PostsList'
 import { AddPostForm } from './features/posts/AddPostForm'
+import { SinglePostPage } from './features/posts/SinglePostPage'
+import { EditPostForm } from './features/posts/EditPostForm'
 
 function App() {
   return (
@@ -20,12 +22,14 @@ function App() {
             exact
             path="/"
             render={() => (
-              <>
-              <AddPostForm/>
-              <PostsList/>
-              </>
+              <React.Fragment>
+                <AddPostForm />
+                <PostsList />
+              </React.Fragment>
             )}
           />
+          <Route exact path="/posts/:postId" component={SinglePostPage} />
+          <Route exact path="/editPost/:postId" component={EditPostForm} />
           <Redirect to="/" />
         </Switch>
       </div>
